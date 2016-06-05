18e28af001050e60d165dc621a2f81340d5a55ff
d.thomas014 <d.thomas014@gmail.com>
2016-06-04 20:02:15 -0400

    bottom social contact links open in new tab

--- js/main.js
+++ js/main.js
@@ -6,22 +6,26 @@ $(".intro .welcome").hide().fadeIn(4400);
 // Portfolio and Contact links should smooth scroll to their correct sections
 
 // Navigation bar links  -- "portfolio" and "contact" 
+// Portfolio button
 $("#btn1").click(function(){
   event.preventDefault();
   $('html, body').animate({scrollTop: $(".sample_work").offset().top}, 1500);    
 });
 
+// Contact button
 $("#btn3").click(function(){
   event.preventDefault();
   $('html, body').animate({scrollTop: $(".contact").offset().top}, 1500);    
 });
 
 // Bottom page links  -- "home" and "portfolio"
+// Home button
 $("#btn4").click(function(){
   event.preventDefault();
   $('html, body').animate({scrollTop: $(".nav_bar").offset().top}, 1500);    
 });
 
+// Portfolio button
 $("#btn5").click(function(){
   event.preventDefault();
   $('html, body').animate({scrollTop: $(".sample_work").offset().top}, 1500);    
@@ -30,13 +34,29 @@ $("#btn5").click(function(){
 
 // Links should open in new tab  -- "apparel" "behance" "linkedin" github"
 
-// "Apparel" link out to behance project page
+// Apparel button
 $('#btn2').click(function() {
   event.preventDefault();
   window.open("https://www.behance.net/collection/76966367/Apparel-Design-Project-Samples");
 });
 
-// "Behance" link out to behance page
+// Behance button
+$('#behance').click(function() {
+  event.preventDefault();
+  window.open("https://behance.net/dthomas");
+});
+
+// LinkedIn button
+$('#linked').click(function() {
+  event.preventDefault();
+  window.open("https://www.linkedin.com/in/dthomas014");
+});
+
+// Github button
+$('#github').click(function() {
+  event.preventDefault();
+  window.open("https://github.com/thom-d");
+});
 
 
 //  lightbox.option({
