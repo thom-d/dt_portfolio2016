d910145251612964af8331fd13294a1af001ba6e
d.thomas014 <d.thomas014@gmail.com>
2016-06-06 16:39:55 -0400

    removed window.open added target=blank for tab open

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 // 1. When the page opens/loads, the name and professional title should fade in.
 // 2. On the navigation bar - when the buttons are clicked, they should smooth scroll accordingly.
-// 3. All link or buttons when clicked should open out into a new tab.
-// 4. When project tiles are clicked, a lightbox should open up. 
+// 3. When project tiles are clicked, a lightbox should open up. 
 
 
 //When doc loads fade in title text.
 $(".intro .welcome").hide().fadeIn(4400);
 
 
-// Portfolio and Contact links should smooth scroll to their correct sections
+// Portfolio and Contact links should smooth scroll to their correct paths
 
 // Navigation bar links  -- "portfolio" and "contact" 
 // Portfolio button
@@ -36,40 +35,20 @@ $("#btn5_portfolio").click(function(e){
   $('body, html').animate({scrollTop: $(".sample_work").offset().top}, 1500);    
 });
 
-
-// Links should open in new tab  -- "apparel" "behance" "linkedin" github"
-
-// Apparel button
-$('#btn2_apparel').click(function(e) {
-  e.preventDefault();
-  window.open("https://www.behance.net/collection/76966367/Apparel-Design-Project-Samples");
-});
-
-// Behance button
-$('#behance').click(function(e) {
-  e.preventDefault();
-  window.open("https://behance.net/dthomas");
-});
-
-// LinkedIn button
-$('#linked').click(function(e) {
-  e.preventDefault();
-  window.open("https://www.linkedin.com/in/dthomas014");
-});
-
-// Github button
-$('#github').click(function(e) {
-  e.preventDefault();
-  window.open("https://github.com/thom-d?tab=repositories");
-});
-
-
 // Portfolio images should open in a lightbox
 
  lightbox.option({
   'positionFromTop': 75,
   'fadeDuration': 200
  });
+
+
+// Code to open window in a new tab
+// Apparel button
+// $('#btn2_apparel').click(function(e) {
+//   e.preventDefault();
+//   window.open("https://www.behance.net/collection/76966367/Apparel-Design-Project-Samples");
+// });
 
 
   // --------- SITE CREATOR INFO ---------
